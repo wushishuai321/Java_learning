add46c61b59e6dbd97444a1bac7e4d7c61297982
wushishuai321 <1261670814@qq.com>
2020-09-12 20:48:33 +0800

    second

---
+++ TestJava_2.java
@@ -0,0 +1,35 @@
+package wushishuai;
+
+public class TestJava_2
+{
+/*    public static void main(String[] args)
+    {
+        boolean a = false;
+        int b = 2;
+        System.out.println("a = "+ a + ", !a = "+(!a));
+        System.out.println("b = "+ b + ", !b = "+(~b));
+    }*/
+/*    public static void main(String[] args)
+    {
+        double a = 5.2;
+        double b = 3.1;
+        System.out.println("5.2%3.1="+a%b);
+    }*/
+
+    //三元运算符
+/*    public static void main(String[] args)
+    {
+        int x = 10;
+        int y = 20;
+
+        int result = x>y?x:y;
+        System.out.println("result="+result);
+
+        x = 50;
+        result = x>y?x:y;
+        System.out.println("result_second="+result);
+
+        char a = 'a';
+        System.out.println(a);
+    }*/
+}
