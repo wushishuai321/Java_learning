69f43aac31d659db1ca39edeec420645203da7eb
wushishuai321 <1261670814@qq.com>
2020-09-13 10:47:07 +0800

    paixu

--- Exercise.java
+++ Exercise.java
@@ -2,7 +2,7 @@ package wushishuai;
 
 public class Exercise
 {
-    public static void main(String[] args)
+/*    public static void main(String[] args)
     {
         //注意rand*58要加括号。
         double rand = Math.random();
@@ -11,5 +11,33 @@ public class Exercise
         System.out.println((char)b);
 
         System.out.println(rand*10);
+    }*/
+
+    public static void main(String[] args)
+    {
+        int[] a= {1,2,45,67,54,35,68,65,98};
+        int temp ;
+
+        for (int i =0;i<a.length;i++)
+        {
+            temp = a[i];
+            int c = 0;
+
+            for(int j = i+1;j<a.length;j++)
+            {
+                if (temp > a[j])
+                {
+                    c =temp;
+                    temp = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+        for(int i = 0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+
     }
 }
